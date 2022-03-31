2d87a6a677f1ec8402123e8f546b1e9c60958e41
vijay.shinde <vijay.shinde@nitorinfotech.com>
2022-03-31 15:04:42 +0530

    Grouping unit tests

--- test/presentation/bloc/ping_bloc_test.dart
+++ test/presentation/bloc/ping_bloc_test.dart
@@ -5,7 +5,6 @@ import 'package:nitor_ping/presentation/bloc/ping_bloc.dart';
 import 'package:nitor_ping/presentation/bloc/ping_event.dart';
 import 'package:nitor_ping/presentation/bloc/ping_state.dart';
 
-
 class FakePingBloc extends MockBloc<PingEvent, PingState> implements PingBloc {}
 
 void main() {
@@ -13,35 +12,39 @@ void main() {
   final Ping mockPing = Ping('google.com', count: 1);
   final states = [PingInitState(), PingingState(mockPing)];
 
-  test('ping emits stream of PingData', () {
-    expect(Ping('google.com', count: 1).stream, emits(isA<PingData>()));
+  group('Ping Class Tests', () {
+    test('ping emits stream of PingData', () {
+      expect(Ping('google.com', count: 1).stream, emits(isA<PingData>()));
+    });
+
+    test('ping emits PingData 5 times', () {
+      expect(
+          Ping('google.com', count: 5).stream,
+          emitsInOrder([
+            isA<PingData>(),
+            isA<PingData>(),
+            isA<PingData>(),
+            isA<PingData>(),
+            isA<PingData>()
+          ]));
+    });
   });
 
-  test('ping emits PingData 5 times', () {
-    expect(
-        Ping('google.com', count: 5).stream,
-        emitsInOrder([
-          isA<PingData>(),
-          isA<PingData>(),
-          isA<PingData>(),
-          isA<PingData>(),
-          isA<PingData>()
-        ]));
+  group('Bloc Tests', () {
+    test('check bloc emits states in order', () {
+      whenListen(mockPingBloc, Stream.fromIterable(states));
+      expectLater(mockPingBloc.stream, emitsInOrder(states));
+    });
+
+    blocTest<PingBloc, PingState>(
+        'When no event is added then no state is emitted',
+        build: () => PingBloc(),
+        expect: () => <PingState>[]);
+
+    blocTest<PingBloc, PingState>(
+        'When button pressed and ping event is added then Pinging State is emitted',
+        build: () => PingBloc(),
+        act: (bloc) => bloc.add(PingPressedEvent(mockPing)),
+        expect: () => <PingState>[PingingState(mockPing)]);
   });
-
-  test('check bloc emits states in order', () {
-    whenListen(mockPingBloc, Stream.fromIterable(states));
-    expectLater(mockPingBloc.stream, emitsInOrder(states));
-  });
-
-  blocTest<PingBloc, PingState>(
-      'When no event is added then no state is emitted',
-      build: () => PingBloc(),
-      expect: () => <PingState>[]);
-
-  blocTest<PingBloc, PingState>(
-      'When button pressed and ping event is added then Pinging State is emitted',
-      build: () => PingBloc(),
-      act: (bloc) => bloc.add(PingPressedEvent(mockPing)),
-      expect: () => <PingState>[PingingState(mockPing)]);
 }
